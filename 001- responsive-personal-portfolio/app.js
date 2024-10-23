199e8069419c4486f021d8b609e3c136fef3b0be
HussienHaji <hussienhaji.dev@gmail.com>
2024-10-23 23:49:15 +0330

    creating the responsive personal portfolio project

---
+++ 001- responsive-personal-portfolio/app.js
@@ -0,0 +1,13 @@
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+  if (navbar.classList.contains("active")) {
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-times");
+  } else {
+    menuIcon.classList.add("fa-bars");
+    menuIcon.classList.remove("fa-times");
+  }
+});
